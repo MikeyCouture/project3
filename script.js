a6a53ca39665c569bdf7f530cb341d43160ddd57
MikeyCouture <michelcouture42@gmail.com>
2018-12-22 21:29:30 -0500

    commiting an unfinished version to help with troubleshooting a JS issue

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 
 const pugApp = {};
 
+// setting up global variables to be used in functions 
 let imageChoice;
 let boxChoice;
 let numChoice;
-let timeoutId;
+let timer;
+let counter = 0;
 
+
+// pugApp init function firing off three others.
 pugApp.init = function () {
     pugApp.listenForImageClick();
     pugApp.listenForBoxClick();
@@ -13,6 +17,7 @@ pugApp.init = function () {
 
 }
 
+// listening for which image the use clicks on. setting image Choice to the pata pic number and adding box shadow
 pugApp.listenForImageClick = function () {
 
     $(".item").on("click", function (event) {
@@ -23,13 +28,13 @@ pugApp.listenForImageClick = function () {
     });
 }
 
+// listening for which box the user has selected. IF the image number and box number match, append to the box.
 pugApp.listenForBoxClick = function () {
 
     $(".box").on("click", function (event) {
         event.preventDefault();
         boxChoice = $(this).attr("data-box");
         console.log(boxChoice);
-        console.log(numChoice);
 
         if (imageChoice === boxChoice) {
             $(`.item${imageChoice}`).appendTo(`.box${boxChoice}`);
@@ -39,16 +44,15 @@ pugApp.listenForBoxClick = function () {
             swal("Try again!", "", "error");
         }
 
+        // if all the divs with images are empty - prompt the user a winning message.
         if ($(".puzzle-Container").children().length == 0 && $(".container-Two").children().length == 0) {
             $(".box").removeClass("box-border");
             $(".main-box").addClass("winner-Highlight");
-            setTimeout(function () {
-                console.log(setTimeout);
-                $(".winner-box").addClass("winner-Display");
-            }, 2500);
-            clearTimeout(timeoutId);
-        }
 
+            setTimeout(function () {
+                $(".winner-box").addClass("winner-Display");
+            }, 2000);
+        }
         $(".play-Again").on("click", function () {
             // $("#pugPic1").attr("src", "./assets/Pug_Image2-1.jpg");
             location.reload();
@@ -58,26 +62,32 @@ pugApp.listenForBoxClick = function () {
 
 pugApp.timeOutAndResize = function () {
 
-    // $(".fas").on("click", function () {
-    //     setTimeout(function () {
-    //         swal("Time is up!", "", "error");
-    //     }, 34000);
-    //     if ($(".puzzle-Container").children().length == 0 && $(".container-Two").children().length == 0) {
-    //         clearTimeout(timeoutId);
-    //     }
-    // });
-
     $(".fas").on("click", function () {
-        timeoutId = setTimeout(function () {
-            swal("Time is up!", "", "error");
-        }, 45000);
+        console.log('clicked!');
+        
+        function timeIt(){
+            counter++;
+            document.getElementById("timer").innerHTML = counter;
+            console.log(counter);
+        }
 
-        // if ($(".puzzle-Container").children().length == 0 && $(".container-Two").children().length == 0) {
-        //     clearTimeout(timeoutId);
-        // }
+        setInterval(timeIt, 1000);
+
+        function myStopFunction() {
+            clearInterval(setInterval);
+        }
+
+        myStopFunction();
+
     });
 
 
+
+    // if ($(".puzzle-Container").children().length == 0 && $(".container-Two").children().length == 0) {
+
+    //     console.log("puzzle finished");
+
+    // };
 
 
 
