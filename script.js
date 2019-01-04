00361f7991c4c32e4c592f1edde07ae66ad260e3
MikeyCouture <michelcouture42@gmail.com>
2019-01-04 12:09:14 -0500

    got timer working and drag/drop function

--- script.js
+++ script.js
@@ -7,6 +7,12 @@ let boxChoice;
 let numChoice;
 let timer;
 let counter = 0;
+let myTimer;
+let picture;
+// const picture = document.querySelector('.item1');
+const gridBox = document.querySelectorAll('.box');
+// console.log(picture);
+
 
 
 // pugApp init function firing off three others.
@@ -14,24 +20,56 @@ pugApp.init = function () {
     pugApp.listenForImageClick();
     pugApp.listenForBoxClick();
     pugApp.timeOutAndResize();
-
 }
+
 
 // listening for which image the use clicks on. setting image Choice to the pata pic number and adding box shadow
 pugApp.listenForImageClick = function () {
 
-    $(".item").on("click", function (event) {
-        event.preventDefault();
+    $(".item").on("dragstart", function () {
+        // event.preventDefault();
         imageChoice = $(this).attr("data-pic");
         console.log(imageChoice);
         $(this).addClass("boxShadow");
+        console.log("started");
     });
+
+    $(".item").on('click', function (event){
+        event.preventDefault();
+    });
+
+    $(".item").on('dragend', function () {
+        console.log("ended");
+    });
+
 }
 
 // listening for which box the user has selected. IF the image number and box number match, append to the box.
 pugApp.listenForBoxClick = function () {
 
-    $(".box").on("click", function (event) {
+    for( empty of gridBox){
+        empty.addEventListener('dragover', dragOver);
+        empty.addEventListener('dragenter', dragEnter);
+        empty.addEventListener('dragleave', dragLeave);
+        // empty.addEventListener('drop', dragDrop);
+    }
+
+    function dragOver(e){
+        e.preventDefault();
+        console.log("over");
+    }
+
+    function dragEnter() {
+        console.log("enter");
+    }
+    function dragLeave() {
+        console.log("leave");
+    }
+    // function dragDrop(){
+    //     console.log("drop");
+    // }
+
+    $(".box").on("drop", function (event) {
         event.preventDefault();
         boxChoice = $(this).attr("data-box");
         console.log(boxChoice);
@@ -48,11 +86,15 @@ pugApp.listenForBoxClick = function () {
         if ($(".puzzle-Container").children().length == 0 && $(".container-Two").children().length == 0) {
             $(".box").removeClass("box-border");
             $(".main-box").addClass("winner-Highlight");
+            console.log("puzzle finished");
 
             setTimeout(function () {
                 $(".winner-box").addClass("winner-Display");
+                // append time on winner box here
+                $(".officialTime").append(counter);
             }, 2000);
         }
+
         $(".play-Again").on("click", function () {
             // $("#pugPic1").attr("src", "./assets/Pug_Image2-1.jpg");
             location.reload();
@@ -62,34 +104,20 @@ pugApp.listenForBoxClick = function () {
 
 pugApp.timeOutAndResize = function () {
 
-    $(".fas").on("click", function () {
+   $(".fas").on("click", function () {
         console.log('clicked!');
-        
-        function timeIt(){
+
+        function timeIt() {
             counter++;
             document.getElementById("timer").innerHTML = counter;
             console.log(counter);
+            if ($(".puzzle-Container").children().length == 0 && $(".container-Two").children().length == 0){
+                clearInterval(myTimer);
+            };
         }
 
-        setInterval(timeIt, 1000);
-
-        function myStopFunction() {
-            clearInterval(setInterval);
-        }
-
-        myStopFunction();
-
+        let myTimer = setInterval(timeIt, 1000);
     });
-
-
-
-    // if ($(".puzzle-Container").children().length == 0 && $(".container-Two").children().length == 0) {
-
-    //     console.log("puzzle finished");
-
-    // };
-
-
 
     $(window).resize(function () {
         if ($(window).width() < 1300) {
